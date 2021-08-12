84b74cb006abc79e0cb6a880ac5027193c1b9249
Jekony-becony <ivanenko231@gmail.com>
2021-08-12 14:38:26 +0300

    Lesson5

---
+++ src/main/java/Fruit.java
@@ -0,0 +1,19 @@
+public class Fruit {
+    public float weight;
+
+    public Fruit() {
+    }
+
+    public Fruit(float weight) {
+
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+}
--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -1,38 +1,57 @@
+import java.util.Scanner;
+
 public class HomeWorkApp {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
+    public static void main(String[] args) {
+        String[] array = new String[]{"First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh"};
+        System.out.println("Massive: ");
+        showArray(array);
+        swapElements(array);
+        Apple apl = new Apple();
+        Orange orange = new Orange();
+        Box<Apple> boxApple = new Box<Apple>();
+        Box<Orange> boxOrange = new Box<Orange>();
+        boxApple.addFruit(apl);
+        boxApple.addFruit(apl);
+        boxApple.addFruit(apl);
+        boxApple.addFruit(apl);
+        boxApple.addFruit(apl);
+        boxOrange.addFruit(orange);
+        boxOrange.addFruit(orange);
+        boxOrange.addFruit(orange);
+
+        boxApple.compare(boxOrange);
+
     }
-    public static void printThreeWords(){
-        System.out.println("Orange!");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
-    public static void checkSumSign(){
-        int a = 1,b = 2;
-        String s = ((a + b) >= 0) ? "—умма положительна€" : "—умма отрицательна€";
-        System.out.println(s);
-    }
-    public static void printColor(){
-        int value =0;
-        if (value<=0)
-        {
-            System.out.println(" расный");
+
+    public static void showArray(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("[" + i + "]: " + array[i]);
         }
-        else if (0<value  && value<=100)
-        {
-            System.out.println("∆елтый");
-        }
-        else { System.out.println("«еленый"); }
     }
-    public static void compareNumbers(){
-        int a = 5,b = 10;
-        if (a>=b){
-            System.out.println("a >= b");
+
+    public static String[] swapElements(String[] array) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("¬ведите номер €чейки первого елемента: ");
+        int number = sc.nextInt();
+        System.out.println("");
+        System.out.print("¬ведите номер €чейки второго елемента: ");
+        int number2 = sc.nextInt();
+        System.out.println("");
+        if (number == number2) {
+            System.out.println("¬ведите два  разных числа!");
+        } else if (number > array.length || number2 > array.length) {
+            System.out.println("„исло выходит за границу массива");
+        } else if (number < 0 || number2 < 0) {
+            System.out.println("«начени€ не могу быть отрицательными!");
+        } else {
+            System.out.println("Before: ");
+            showArray(array);
+            String k = array[number];
+            array[number] = array[number2];
+            array[number2] = k;
+            System.out.println("After: ");
+            showArray(array);
         }
-        else{System.out.println("a < b");}
+        return array;
     }
 }
